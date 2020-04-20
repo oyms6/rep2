be071a425fa54adeba81cea3fb16e5ec9f8dc43f
oyms <1358804630@qq.com>
2020-04-21 01:32:47 +0800

    ct1

--- untitled7/src/com/company/Main.java
+++ untitled7/src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.println(1);
         System.out.println(2);
         System.out.println(3);
-        System.out.println("xiugaile);
+        System.out.println("ct1);
 		
     }
 }
